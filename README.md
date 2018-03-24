bf05d56c7c2b96313a3383931c898ca6b499f8ff
DESKTOP-MLO5OLP\Gian <gian.d.clemons@gmail.com>
2018-03-24 13:35:58 -0500

    save point before cheking out jeremy branch

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # musician-finder-app
-App that is designed for other musicians to find one another. 
+Is  designed for musicians to find one another.
+
+index.html is the login/sign-up page for current and future users
+profile.html is the page that will show all of the current musicians in the area. 
+
+TODO
+*Authentication
+
+ 
--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,8 @@
 var maxFM_Results = 10;
 
-// last FM query
-// query on click
-$("#me").on("click", function() {
-  //get artists name from profile currently testing
-  var artist = "metallica";
+// last FM query fires automatically on page load. 
+  //get artists name in Profile
+  var artist = $("#artist-profile").text();
 
   // last FM keys
   var APIkey = "939610d3ede414a19a0eef9dd79b91ec";
@@ -16,6 +14,7 @@ $("#me").on("click", function() {
     APIkey +
     "&format=json";
 
+//Query is for top tracks
   $.ajax({
     url: lastFMURL,
     method: "GET"
@@ -24,54 +23,11 @@ $("#me").on("click", function() {
     //loops and prints out top 10 tracks and the albums
     for(var count = 0; count < maxFM_Results; count++){
 
-      // console.log(count, " Track: ",response.toptracks.track[count].name);
       var newTrack = $("<p>");
       newTrack.html((count+1) + ". "+ response.toptracks.track[count].name);
       $("#top-tracks").append(newTrack);
     }
   });
-});
-
-// var Latitude = "";
-// var Longitude = "";
-// var zip = "75248";
-// var url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + zip;
-// console.log(url)
-// //creates map
-// function initMap() {
-//     $.ajax({
-//         url: url,
-//         method: "GET"
-//     })
-//         .then(function (response) {
-//             Latitude = response.results[0].geometry.location.lat;
-//             Longitude = response.results[0].geometry.location.lng;
-//             console.log(Latitude, Longitude);
-//             var latlong = { lat: Latitude, lng: Longitude };
-//             console.log(latlong)
-//             var map = new google.maps.Map(document.getElementById('map'), {
-//                 zoom: 9,
-//                 center: latlong
-//             });
-//             //creates marker
-//             var marker = new google.maps.Marker({
-//                 position: latlong,
-//                 map: map
-//             });
-//             //creates circle
-//             var circle = new google.maps.Circle({
-//                 map: map,
-//                 radius: 40233.6,    // 10 miles in metres
-//                 fillColor: '#AA0000'
-//             });
-//             circle.bindTo('center', marker, 'position');
-//         }
-//         );
-//     };
-
-
-
-
 
 // Initialize Firebase (Gian's firebase)
 var config = {
@@ -84,23 +40,7 @@ var config = {
   };
   firebase.initializeApp(config);
 
-// code for saving login data to firebase
 var database = firebase.database();
-
-$("#loginbutton").click(function(e){
-    e.preventDefault();
-    var email = $("#email").val();
-    var password = $("#password").val();
-    console.log("email ",email, " password ", password);
-    loginResult = checkLogin(username, password);
-    window.location = "profile.html";    
-
-    // clear input boxes
-    $("#email").val("");
-    $("#password").val("");
-    
- 
-});
 
 $("#createbutton").click(function(e){
 
@@ -133,3 +73,7 @@ $("#createbutton").click(function(e){
     // opens profile windows
       window.location = "profile.html";    
 });
+
+// select overdiv
+// select id's through loop
+// run ajax on each element. 
